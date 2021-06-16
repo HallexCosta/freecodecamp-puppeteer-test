7644692a669670731f8b69284e537ade7eee334e
HallexCosta <hallex.costa@hotmail.com>
2021-06-16 00:18:36 -0300

    chore: fix bugs

--- example.spec.js
+++ example.spec.js
@@ -1,11 +1,11 @@
-jest.useFakeTimers();
 const puppeteer = require("puppeteer");
+
 class Example {
   async start() {
     const browser = await puppeteer.launch({
       args: ["--no-sandbox"],
       executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
-      headless: process.env.CI == "true",
+      headless: true,
     });
     const page = await browser.newPage();
 
@@ -38,23 +38,37 @@ describe("Jest Puppeteer test", () => {
   it(
     'Should be able get title "Assistir Lista de Animes - Online em FHD"',
     (done) => {
-      example.start().then(() => {
-        expect(example.title).toBe("Assistir Lista de Animes - Online em FHD");
-        done();
-      });
+      example
+        .start()
+        .then(() => {
+          expect(example.title).toBe(
+            "Assistir Lista de Animes - Online em FHD"
+          );
+          done();
+        })
+        .catch((e) => {
+          console.log(e);
+          done();
+        });
     },
-    1000 * 60 * 30
+    10000 * 60 * 3
   );
 
   it(
     "Should be able get animes names",
     (done) => {
-      example.start().then(() => {
-        expect(typeof example.names).toBe("object");
-        expect(example.names.length > 0).toBe(true);
-        done();
-      });
+      example
+        .start()
+        .then(() => {
+          expect(typeof example.names).toBe("object");
+          expect(example.names.length > 0).toBe(true);
+          done();
+        })
+        .catch((e) => {
+          console.log(e);
+          done();
+        });
     },
-    1000 * 60 * 30
+    10000 * 60 * 30
   );
 });
