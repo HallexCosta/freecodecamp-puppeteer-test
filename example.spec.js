dba32ec1d49becf534131bd03ca369ee75daaa13
HallexCosta <hallex.costa@hotmail.com>
2021-06-16 21:45:09 -0300

    chore: move setUserAgent before goto page

--- example.spec.js
+++ example.spec.js
@@ -11,13 +11,13 @@ class Example {
 
     const uri = `https://yayanimes.net/lista-de-animes`;
 
+    await page.setUserAgent(
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+    );
     await page.goto(uri, {
       timeout: 0,
       waitUntil: "networkidle2",
     });
-    await page.setUserAgent(
-      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-    );
 
     this.title = await page.title();
 
